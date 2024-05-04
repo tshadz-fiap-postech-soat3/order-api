47cca97d604d22cf8bed83bcf0f100f0bebea3df
tshadz <thais.sombra@hotmail.com>
2024-05-04 17:24:54 -0300

    fix: fix unit tests

--- test/unit/categories/order-items.api.spec.ts
+++ test/unit/categories/order-items.api.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OrderItemsController } from '../../../src/external/driver/order-items.api';
+import { OrderItemsApi } from '../../../src/external/driver/order-items.api';
 import { OrderItemsService } from '../../../src/@core/order-item/order-items.service';
 import { PrismaService } from '../../../src/external/driven/infra/database/prisma.service';
 import { IOrderItemsRepository } from '../../../src/@core/order-item/repositories/iorder-items.repository';
 import { PrismaOrderItemsRepository } from '../../../src/@core/order-item/repositories/prisma-order-items-repository';
+import { OrderItemsController } from '../../../src/@core/order-item/controller/order-item.controller';
+import { IOrderItemsService } from '../../../src/@core/order-item/iorderItems.service';
 
-describe('OrderItemsController', () => {
-  let controller: OrderItemsController;
+describe('OrderItemsApi', () => {
+  let controller: OrderItemsApi;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [OrderItemsController],
+      controllers: [OrderItemsApi],
       providers: [
-        OrderItemsService,
+        OrderItemsController,
+        {
+          provide: IOrderItemsService,
+          useClass: OrderItemsService,
+        },
         {
           provide: IOrderItemsRepository,
           useClass: PrismaOrderItemsRepository,
@@ -21,7 +27,7 @@ describe('OrderItemsController', () => {
       ],
     }).compile();
 
-    controller = module.get<OrderItemsController>(OrderItemsController);
+    controller = module.get<OrderItemsApi>(OrderItemsApi);
   });
 
   it('should be defined', () => {
--- test/unit/categories/orders.api.spec.ts
+++ test/unit/categories/orders.api.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OrdersController } from '../../../src/external/driver/orders.api';
+import { OrdersApi } from '../../../src/external/driver/orders.api';
 import { OrdersService } from '../../../src/@core/order/orders.service';
 import { IOrdersRepository } from '../../../src/@core/order/repositories/iorder.repository';
 import { PrismaOrdersRepository } from '../../../src/@core/order/repositories/prisma-orders-repository';
 import { PrismaService } from '../../../src/external/driven/infra/database/prisma.service';
+import { OrdersController } from '../../../src/@core/order/controller/orders.controller';
+import { IOrdersService } from '../../../src/@core/order/iorders.service';
+import { IOrdersController } from '../../../src/@core/order/controller/iorders-controller';
 
-describe('OrdersController', () => {
-  let controller: OrdersController;
+describe('OrdersApi', () => {
+  let controller: OrdersApi;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [OrdersController],
+      controllers: [OrdersApi],
       providers: [
-        OrdersService,
+        {
+          provide: IOrdersController,
+          useClass: OrdersController,
+        },
+        {
+          provide: IOrdersService,
+          useClass: OrdersService,
+        },
         {
           provide: IOrdersRepository,
           useClass: PrismaOrdersRepository,
@@ -21,7 +31,7 @@ describe('OrdersController', () => {
       ],
     }).compile();
 
-    controller = module.get<OrdersController>(OrdersController);
+    controller = module.get<OrdersApi>(OrdersApi);
   });
 
   it('should be defined', () => {
