901ad56d165e8fbad88b36d37003c4ab46ddba0c
Alan Bonachella <alan.bonachella@gmail.com>
2024-05-11 16:54:19 -0300

    feat: remove order item api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { OrdersModule } from './@core/order/orders.module';
 import { DatabaseModule } from './external/driven/infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { OrderItemsModule } from './@core/order-item/order-items.module';
 
 @Module({
   imports: [
     OrdersModule,
     DatabaseModule,
-    ConfigModule.forRoot(),
-    OrderItemsModule,
+    ConfigModule.forRoot()
   ],
 })
 export class AppModule {}
