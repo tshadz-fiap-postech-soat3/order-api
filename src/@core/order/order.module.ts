4a1a36a9e967112892c1180c2bf19591f96b0b89
Alan Bonachella <alan.bonachella@gmail.com>
2024-05-15 00:26:46 -0300

    feat: rename order status to order strategies

--- src/@core/order/order.module.ts
+++ src/@core/order/order.module.ts
@@ -4,7 +4,7 @@ import { OrdersApi } from '../../external/driver/orders.api';
 import { InfraModule } from '../../external/driven/infra/infra.module';
 import { OrderServiceProvider } from './services/order-service.provider';
 import { OrderControllerProvider } from './controller/order-controller.provider';
-import { OrderStrategiesModule } from './services/order-status-strategies/order-strategies.module';
+import { OrderStrategiesModule } from './services/order-strategies/order-strategies.module';
 
 @Module({
   imports: [InfraModule, OrderStrategiesModule],
